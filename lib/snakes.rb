157faf6d1fa78484dd3f2c22c23e3f04fd7e5d98
Mark24 <mark.zhangyoung@qq.com>
2022-01-05 23:27:22 +0800

    init

---
+++ lib/snakes.rb
@@ -0,0 +1,9 @@
+# frozen_string_literal: true
+
+require_relative "snakes/version"
+
+module Snakes
+  class Error < StandardError; end
+  puts "hello world this is snakes game"
+  # Your code goes here...
+end
